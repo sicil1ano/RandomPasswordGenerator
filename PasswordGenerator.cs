f0288f60e1007f5a8e7c0f1f739f7ec8d75f39c0
Alberto Solano <ajxweb571@gmail.com>
2013-05-11 12:36:20 +0200

    First commit
    Added files

---
+++ PasswordGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        public string createPassword()
+        {
+            string strPwdchar = "abcdefghijklmnopqrstuvwxyz0123456789#+@&$!?*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int charNum = 8;
+            StringBuilder key = new StringBuilder(charNum);
+            Random rmNum = new Random();
+            int number = rmNum.Next(1, 5000);
+            for (int i = 0; i <= charNum; i++)
+            {
+                int randNum = rmNum.Next(0, strPwdchar.Length - 1);
+                key.Append(strPwdchar.Substring(randNum, 1));
+            }
+
+            key.Append(twoChars(2));
+            key.Append(number);
+
+
+            return key.ToString();
+        }
+
+        private String twoChars(int size)
+        {
+            StringBuilder stringObtained = new StringBuilder();
+            Random rm = new Random();
+
+            char charKey;
+
+            for (int i = 0; i < size; i++)
+            {
+                charKey = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * rm.NextDouble() + 65)));
+                stringObtained.Append(charKey);
+            }
+
+            return stringObtained.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static string newPassword;
+        static PasswordGenerator passGen;
+        static bool newPasswordCreation = true;
+        static bool exitApplication;
+
+        [STAThread] // this is neeeded to avoid the exception : "Current thread must be set to single thread apartment (sta) mode before ole calls can be made"
+        //just when executing System.Windows.Forms.Clipboard.SetText(); to copy the password to clipboard!
+        static void Main(string[] args)
+        {
+            while (!exitApplication && newPasswordCreation)
+            {
+                DisplayWelcomeMessage();
+                Console.WriteLine("A strong password must have at least 8 characters.");
+                newPassword = PasswordCreation();
+                DisplayPassword(newPassword);
+                ShowCopyToClipboardHint();
+                CreateNewPasswordHint();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the application..");
+            Console.ReadKey();
+        }
+
+        static void DisplayWelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("RANDOM PASSWORD GENERATOR!");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White; 
+        }
+
+        static void DisplayPassword(string password)
+        {
+            Console.WriteLine("New random password created! --> " + password);
+        }
+
+        static string PasswordCreation()
+        {
+            passGen = new PasswordGenerator();
+            string password = passGen.createPassword();
+            return password;
+        }
+
+        static void CreateNewPasswordHint()
+        {
+            Console.WriteLine("Would you like to create a new random password?");
+            Console.WriteLine("Y : Yes - N: No, thanks!");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.Write("\b");
+            switch (key.Key)
+            {
+                case ConsoleKey.Y:
+                    newPasswordCreation = true;
+                    Console.Clear();
+                    break;
+
+                case ConsoleKey.N:
+                    Console.WriteLine("Ok, another password won't be created. The application will exit..");
+                    newPasswordCreation = false;
+                    exitApplication = true;
+                    break;
+                default:
+                    Console.WriteLine("Wrong key pressed, man!");
+                    break;
+            }
+        }
+
+        static void ShowCopyToClipboardHint()
+        {
+            Console.WriteLine("Do you want to copy the password to the clipboard?");
+            Console.WriteLine("Y : Yes - N: No, thanks!");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.Write("\b");
+            switch (key.Key)
+            {
+                case ConsoleKey.Y:
+                    CopyPasswordToClipboard(newPassword);
+                    Console.WriteLine("The password has been copied to your clipboard!");
+                    break;
+
+                case ConsoleKey.N:
+                    Console.WriteLine("Ok, no password copied!");
+                    break;
+                default:
+                    Console.WriteLine("Wrong key pressed, man!");
+                    break;
+            }
+        }
+
+        static void CopyPasswordToClipboard(string stringToCopy)
+        {
+            System.Windows.Forms.Clipboard.SetText(stringToCopy);
+        }
+
+    }
+}
